d90af98f2e9a81e828ec5d829ab1e78a0dcaf7b8
bronifty <bronifty@gmail.com>
2023-05-07 18:02:13 -0400

    this

---
+++ sanity/src/structure/index.js
@@ -0,0 +1,18 @@
+export const structure = (S, context) =>
+  S.list()
+    .title('Site Content!!')
+    .items([
+      S.listItem()
+        .title('Site Settings')
+        .child(S.editor().id('siteSettings').schemaType('siteSettings').documentId('siteSettings')),
+      ...S.documentTypeListItems().filter(
+        (listItem) =>
+          ![
+            'siteSettings',
+            // 'post',
+            // 'author',
+            // 'category',
+            // 'pet'
+          ].includes(listItem.getId())
+      ),
+    ])
